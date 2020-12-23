2c44d57862ae0ff3933a52a732a043df45a9ed92
edgar <edgargc.upc@gmail.com>
2020-12-23 16:22:32 +0100

    Demium info

--- src/views/UDashboard/Components/HackViews/Demium.js
+++ src/views/UDashboard/Components/HackViews/Demium.js
@@ -7,7 +7,7 @@ import demium from "./Assets/DemiumView.png";
 export const Demium = () => {
     const evento = {
         title: "Hack that startup",
-        description: "Primera competición de hacking que te permite entrar en el mundo de las startups",
+        description: "En esta primera edición de Hack that startap te propomenos diferentes retos relaciondos con las startups incubadas en Demium Barcelona. Podrás elegir entre dos disciplinas: Data Science & IA y Desarrollo web. Tendrás la oportunidad de ganar un cheque de Amazon para que puedas seguir progresando y a los 10 mejores los pondremos en contacto con las startups de Demium que buscan perfiles tech. Ponte en el foco de las Startups más top de Barcelona. ¿Estás preparado?",
         image: demium,
         link: "https://discord.gg/h4nqE3xSd6",
         register: "Sólo tienes que darle al botón y automáitcamente se registará el evento en tu perfil y podrás unirte al canal.",
